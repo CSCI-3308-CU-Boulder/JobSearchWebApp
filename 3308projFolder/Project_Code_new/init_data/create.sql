9522ad4860df1b4df0e8d299aecf00c5d02bd191
zoecruse <41089494+zoecruse@users.noreply.github.com>
2021-04-11 00:22:04 -0600

    added everything but node_modules
     bc i cant figure out how to upload that many files

---
+++ 3308projFolder/Project_Code_new/init_data/create.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS profile;
+CREATE TABLE IF NOT EXISTS profile (
+  full_name VARCHAR(30),    
+  username VARCHAR(30) NOT NULL,  
+  password_ VARCHAR(30) NOT NULL,
+  major VARCHAR(30),
+  gpa DECIMAL(3,2),       
+  year  VARCHAR(30),
+  pronouns VARCHAR(30),
+  experience VARCHAR(500),
+  PRIMARY KEY(username) 
+);
+
+
+
+INSERT INTO profile(full_name, username, password_, major, gpa, year, pronouns, experience)
+VALUES('Abeal Sileshi', 'abealsileshi', 'fakepassword', 'CS', 4.00, 'senior' , 'he/him/his', 'I have minimal experience.'); 
