281479528d36063aa317850ac43fbbddb6fb01b9
a11isonliu <allison.liu@colorado.edu>
2021-04-13 15:00:43 -0600

    re-linked home, profile, settings page

--- ProjectCode_v3/resources/js/home.js
+++ ProjectCode_v3/resources/js/home.js
@@ -2,3 +2,27 @@ function darkMode(){
     var bg_color = document.body;
     bg_color.classList.toggle("dark-mode");
 }
+
+// Load the modules
+var express = require('express'); //Express - a web application framework that provides useful utility functions like 'http'
+var app = express();
+var bodyParser = require('body-parser'); // Body-parser -- a library that provides functions for parsing incoming requests
+app.use(bodyParser.json());              // Support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // Support encoded bodies
+const axios = require('axios');
+const qs = require('query-string');
+
+// Set the view engine to ejs
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/'));// Set the relative path; makes accessing the resource directory easier
+
+
+// Home page - DON'T CHANGE
+app.get('/', function(req, res) {
+  res.render('pages/home', {
+    my_title: "CareerNetwork",
+    items: '',
+    error: false,
+    message: ''
+  });
+});
